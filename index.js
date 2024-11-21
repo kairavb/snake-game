a3d8085a75884fdcf46b9a5754c7e8b1f00dcc66
kairavb <kairavb8@gmail.com>
2024-11-21 19:12:22 +0530

    added game logic

---
+++ index.js
@@ -0,0 +1,13 @@
+let direction = {x: 0, y: 0};
+let speed = 5;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x: 10, y: 10}
+];
+
+food = {x: 5, y: 5};
+
+function main() {}
+function isCollide() {}
+function gameEngine() {}
