d95674c53eb8875538ffc623f4b0a45cbd31c5f9
kairavb <kairavb8@gmail.com>
2024-11-21 22:52:41 +0530

    logic up

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let blocksize = 25
 let rows = 20
+let cols = 20
 let board
 let context
+
+let snakeX = blocksize * 5
+let snakeY = blocksize * 5
+
+let velX = 0
+let velY = 0
+
+let foodX
+let foodY
 
 window.onload = ()=>{
     board = document.getElementById("board")
@@ -9,5 +19,27 @@ window.onload = ()=>{
     board.width = cols * blocksize
     context = board.getContext("2d");
 
-    update();
+    placeFood()
+    document.addEventListener("keyup", changeDirection)
+    update()
+}
+
+function update() {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, board.width, board.height)
+
+    context.fillStyle = "lime";
+    context.fillRect(snakeX, snakeY, blocksize, blocksize)
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blocksize, blocksize)
+}
+
+function changeDirection(e) {
+
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blocksize
+    foodY = Math.floor(Math.random() * rows) * blocksize
 }
